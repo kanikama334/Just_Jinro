fdb61024a603c3111f0d00b57bc39e1c45ab5b17
ibuki <kamo02903@gmail.com>
2024-10-12 16:59:25 +0900

    first Commit

---
+++ Assets/UdonSharp/EntryButton.cs
@@ -0,0 +1,24 @@
+﻿
+using BestHTTP.SecureProtocol.Org.BouncyCastle.Asn1.Ocsp;
+using TMPro;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class EntryButton : OriginalUdonSharpBehaviour
+{   
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField, UdonSynced] int players; 
+
+    public void OnClicked(){
+        udonSyncedVariable.players++;
+        SerializeRequest();
+    }
+
+    void Update(){
+        text.text = "players: " + players.ToString();
+    }
+}
---
+++ Assets/UdonSharp/UdonSyncedVariable.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class UdonSyncedVariable : UdonSharpBehaviour
+{
+    [SerializeField] public int players;
+
+    // オーナーにRequestSerializationを呼び出してもらうメソッド
+    public void CallRequestSerialization(){
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.Owner, nameof(CallRequestSerializationMethod));
+    }
+    public void CallRequestSerializationMethod(){
+        RequestSerialization();
+    }
+} 
